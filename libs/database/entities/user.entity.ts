28bc12d68399e8197e860936cb794fe082443204
Gwon Min Je <gwon713@gmail.com>
2021-10-26 22:43:41 +0900

    feat: user entity

---
+++ libs/database/entities/user.entity.ts
@@ -0,0 +1,44 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+export enum userSocialType {
+    KAKAO='kakao',
+    GOOGLE='google',
+    GITHUB='github'
+}
+@Entity('ddudo_user')
+export class DdudoUserEntity extends BaseEntity{
+    @PrimaryGeneratedColumn('uuid')
+    uuid: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    nickname: string;
+
+    @Column()
+    token: string;
+
+    @Column()
+    social: userSocialType;
+
+    @Column()
+    file: string;
+    
+    @UpdateDateColumn({
+        type: 'timestamp'
+    })
+    updated_at: string;
+
+    @CreateDateColumn({
+        type: 'timestamp'
+    })
+    created_at: string;
+
+    @Column({
+        type: 'timestamp'
+    })
+    last_login_at: string;
+
+    @Column()
+    deleted: boolean;
+}
