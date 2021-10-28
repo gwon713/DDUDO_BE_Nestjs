ca714698e1f338e977b5ef5f43b0af66f395d8ff
Gwon Min Je <gwon713@gmail.com>
2021-10-28 22:51:11 +0900

    feat: group entity

--- libs/database/entities/group.entity.ts
+++ libs/database/entities/group.entity.ts
@@ -1,7 +1,56 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('group')
 export class DdudoGroupEntity extends BaseEntity{
     @PrimaryGeneratedColumn('uuid')
     uuid: string;
+
+    @Column()
+    user_id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    kind: string;
+
+    @Column()
+    status: boolean;
+
+    @Column()
+    liked: number;
+
+    @Column()
+    start_date: string;
+
+    @Column()
+    place: string;
+
+    @Column()
+    chat_link: string;
+
+    @Column()
+    total_user: number;
+
+    @Column()
+    join_user: number;
+
+    @Column()
+    about: string;
+
+    @Column()
+    hashtag: any;
+
+    @UpdateDateColumn({
+        type: 'timestamp'
+    })
+    updated_at: string;
+
+    @CreateDateColumn({
+        type: 'timestamp'
+    })
+    created_at: string;
+
+    @Column()
+    deleted: boolean;
 }
