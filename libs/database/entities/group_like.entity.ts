c68f16005f5c2e4c15d5761f0873c49a0cc28ca1
Lukas-ateam <gwon713@gmail.com>
2021-11-02 20:12:57 +0900

    feat: group like init

---
+++ libs/database/entities/group_like.entity.ts
@@ -0,0 +1,6 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('group_like')
+export class DdudoGroupLikeEntity extends BaseEntity{
+    
+}
