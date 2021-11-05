6f92767e741c510024208fdf67cc9bf93b66a05a
Gwon Min Je <gwon713@gmail.com>
2021-11-05 22:34:56 +0900

    feat: common code

---
+++ libs/database/entities/code.entity.ts
@@ -0,0 +1,4 @@
+import { BaseEntity, Entity } from 'typeorm';
+
+@Entity('code')
+export class DdudoCodeEntity extends BaseEntity {}
---
+++ libs/database/entities/code_detail.entity.ts
@@ -0,0 +1,4 @@
+import { BaseEntity, Entity } from 'typeorm';
+
+@Entity('code_detail')
+export class DdudoCodeDetailEntity extends BaseEntity {}
