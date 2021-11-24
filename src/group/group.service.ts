748bd3a228c8a95a831cddea605efbe5d25116f3
Gwon Min Je <gwon713@gmail.com>
2021-11-24 22:26:08 +0900

    feat: init group service

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -5,4 +5,32 @@ export class GroupService {
   getHealth(): string {
     return 'group health';
   }
+
+  groupList(): string {
+    return 'group list';
+  }
+
+  groupDetail(): string {
+    return 'group detail';
+  }
+
+  groupAdd(): string {
+    return 'group add';
+  }
+
+  groupEditLiked(): string {
+    return 'group edit liked';
+  }
+
+  groupEditStatus(): string {
+    return 'group edit status';
+  }
+
+  groupEdit(): string {
+    return 'group edit';
+  }
+
+  getMyGroup(): string {
+    return 'get my group';
+  }
 }
