ab2488e4c3b23bdf9e0babc6c0e13bb0a1da0325
Gwon Min Je <gwon713@gmail.com>
2021-11-26 21:08:09 +0900

    feat: swagger set

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { setupSwagger } from 'libs/utils/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  Logger.log('NODE ENV: '+process.env.NODE_ENV);
+  Logger.log('NODE ENV: ' + process.env.NODE_ENV);
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
