a049591df0c146bbdd512fd597c82c48a9c2ffaf
Gwon Min Je <gwon713@gmail.com>
2021-12-01 21:50:50 +0900

    fix: swagger

--- libs/utils/swagger.ts
+++ libs/utils/swagger.ts
@@ -8,8 +8,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
  */
 export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
-    .setTitle('NestJS Study API Docs')
-    .setDescription('NestJS Study API description')
+    .setTitle('Ddudo NestJS API Docs')
+    .setDescription('Ddudo NestJS API description')
     .setVersion('1.0.0')
     .build();
 
