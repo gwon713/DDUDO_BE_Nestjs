f2624812e46987deaa04dcc1d2f041fd2342af04
Gwon Min Je <gwon713@gmail.com>
2021-12-06 21:40:23 +0900

    feat: gql

--- libs/database/database.module.ts
+++ libs/database/database.module.ts
@@ -4,6 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DdudoUserEntity } from './entities/user.entity';
 import { DdudoGroupEntity } from './entities/group.entity';
 import { GraphQLModule } from '@nestjs/graphql';
+import { DdudoCodeDetailEntity } from './entities/code_detail.entity';
+import { DdudoCodeEntity } from './entities/code.entity';
+import { DdudoGroupLikeEntity } from './entities/group_like.entity';
 
 @Module({
   imports: [
@@ -18,10 +21,19 @@ import { GraphQLModule } from '@nestjs/graphql';
       username: process.env.PG_USER,
       database: process.env.PG_DATABASE,
       password: process.env.PG_PASSWORD,
-      entities: [DdudoUserEntity, DdudoGroupEntity],
+      entities: [
+        DdudoUserEntity,
+        DdudoGroupEntity,
+        DdudoGroupLikeEntity,
+        DdudoCodeEntity,
+        DdudoCodeDetailEntity,
+      ],
       synchronize: true,
     }),
-    GraphQLModule.forRoot({}),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
   ],
 })
 export class DatabaseModule {}
