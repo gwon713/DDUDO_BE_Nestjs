92e58863a2825b2e58d1d933c831ca405f2eb861
Gwon Min Je <gwon713@gmail.com>
2021-12-10 23:26:05 +0900

    feat: user model

--- libs/common/models/user.ts
+++ libs/common/models/user.ts
@@ -1,136 +1,35 @@
-/*
-import { Logger } from "@nestjs/common";
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { userSocialType } from 'libs/database/entities/user.entity';
 
+@ObjectType()
 export class User {
-    private seq : Number;
-    private email : String;
-    private nickname : String;
-    private token : String;
-    private social : String;
-    private file : String;
-    private login_time : String;
-    private edit_time : String;
-    private join_time : String;
-    private deleted : Boolean;
+  @Field(() => ID, { nullable: false })
+  id!: string;
 
-    constructor(
-        seq: Number, 
-        email: String, 
-        nickname: String, 
-        token: String, 
-        social: String, 
-        file: String, 
-        login_time: String, 
-        edit_time: String, 
-        join_time: String, 
-        deleted: Boolean
-    )
-    {
-        this.seq = seq
-        this.email = email
-        this.nickname = nickname
-        this.token = token
-        this.social = social
-        this.file = file
-        this.login_time = login_time
-        this.edit_time = edit_time
-        this.join_time = join_time
-        this.deleted = deleted
-    }
+  @Field(() => String, { nullable: false })
+  email!: string;
 
-    public getSeq(): Number {
-        return this.seq;
-    }
+  @Field(() => String, { nullable: false })
+  nickname!: string;
 
-    public setSeq(seq: Number): void {
-        this.seq = seq;
-    }
+  @Field(() => String, { nullable: false })
+  token!: string;
 
-    public getEmail(): String {
-        return this.email;
-    }
+  @Field(() => userSocialType, { nullable: false })
+  social!: userSocialType;
 
-    public setEmail(email: String): void {
-        this.email = email;
-    }
+  @Field(() => String, { nullable: true })
+  file?: string;
 
-    public getNickname(): String {
-        return this.nickname;
-    }
+  @Field(() => Date, { nullable: false })
+  updated_at!: Date;
 
-    public setNickname(nickname: String): void {
-        this.nickname = nickname;
-    }
+  @Field(() => Date, { nullable: false })
+  created_at!: Date;
 
-    public getToken(): String {
-        return this.token;
-    }
+  @Field(() => Date, { nullable: false })
+  last_login_at!: Date;
 
-    public setToken(token: String): void {
-        this.token = token;
-    }
-
-    public getSocial(): String {
-        return this.social;
-    }
-
-    public setSocial(social: String): void {
-        this.social = social;
-    }
-
-    public getFile(): String {
-        return this.file;
-    }
-
-    public setFile(file: String): void {
-        this.file = file;
-    }
-
-    public getLogin_time(): String {
-        return this.login_time;
-    }
-
-    public setLogin_time(login_time: String): void {
-        this.login_time = login_time;
-    }
-
-    public getEdit_time(): String {
-        return this.edit_time;
-    }
-
-    public setEdit_time(edit_time: String): void {
-        this.edit_time = edit_time;
-    }
-
-    public getJoin_time(): String {
-        return this.join_time;
-    }
-
-    public setJoin_time(join_time: String): void {
-        this.join_time = join_time;
-    }
-
-    public isDeleted(): Boolean {
-        return this.deleted;
-    }
-
-    public setDeleted(deleted: Boolean): void {
-        this.deleted = deleted;
-    }
-
-    printInfoUser = (): void => {
-        Logger.log({
-            user_seq : this.seq,
-            user_email : this.email,
-            user_nickname : this.nickname,
-            user_token : this.token,
-            user_social : this.social,
-            user_file : this.file,
-            user_login_time : this.login_time,
-            user_edit_time : this.edit_time,
-            user_join_time : this.join_time,
-            user_deleted : this.deleted
-        });
-    }
+  @Field(() => Boolean, { nullable: false })
+  deleted!: boolean;
 }
-*/
