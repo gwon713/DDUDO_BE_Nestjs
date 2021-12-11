89f97a29a99e01e8f45495ff8e65464c51c3a28f
Gwon Min Je <gwon713@gmail.com>
2021-12-11 17:53:30 +0900

    feat: group model

--- libs/common/models/group/group.ts
+++ libs/common/models/group/group.ts
@@ -1,207 +1,52 @@
-/*
-import { Logger } from "@nestjs/common";
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
-export class Group{
-    private seq : Number;
-    private user_seq : Number;
-    private name : String;
-    private kind : String;
-    private status : Boolean;
-    private liked : Number;
-    private start_date : String;
-    private place : String;
-    private chat_link : String;
-    private total_user : Number;
-    private join_user : Number;
-    private about : String;
-    private hashtag : Object;
-    private edit_time : String;
-    private create_time : String;
-    private deleted : Boolean;
+@ObjectType()
+export class DdudoGroup {
+  @Field(() => ID, { nullable: false })
+  id!: string;
 
-    constructor(
-        seq: Number, 
-        user_seq: Number, 
-        name: String, 
-        kind: String, 
-        status: Boolean, 
-        liked: Number, 
-        start_date: String, 
-        place: String, 
-        chat_link: String, 
-        total_user: Number, 
-        join_user: Number, 
-        about: String, 
-        hashtag: Object, 
-        edit_time: String, 
-        create_time: String, 
-        deleted: Boolean
-    )   
-    {
-        this.seq = seq
-        this.user_seq = user_seq
-        this.name = name
-        this.kind = kind
-        this.status = status
-        this.liked = liked
-        this.start_date = start_date
-        this.place = place
-        this.chat_link = chat_link
-        this.total_user = total_user
-        this.join_user = join_user
-        this.about = about
-        this.hashtag = hashtag
-        this.edit_time = edit_time
-        this.create_time = create_time
-        this.deleted = deleted
-    }
+  @Field(() => String, { nullable: false })
+  userId!: string;
 
-    public getSeq(): Number {
-        return this.seq;
-    }
+  @Field(() => String, { nullable: false })
+  name!: string;
 
-    public setSeq(seq: Number): void {
-        this.seq = seq;
-    }
+  @Field(() => String, { nullable: false })
+  kind!: string;
 
-    public getUser_seq(): Number {
-        return this.user_seq;
-    }
+  @Field(() => Boolean, { nullable: false })
+  status!: boolean;
 
-    public setUser_seq(user_seq: Number): void {
-        this.user_seq = user_seq;
-    }
+  @Field(() => Number, { nullable: true })
+  liked?: number;
 
-    public getName(): String {
-        return this.name;
-    }
+  @Field(() => String, { nullable: false })
+  start_date!: string;
 
-    public setName(name: String): void {
-        this.name = name;
-    }
+  @Field(() => String, { nullable: false })
+  place!: string;
 
-    public getKind(): String {
-        return this.kind;
-    }
+  @Field(() => String, { nullable: false })
+  chat_link!: string;
 
-    public setKind(kind: String): void {
-        this.kind = kind;
-    }
+  @Field(() => Number, { nullable: false })
+  total_user!: number;
 
-    public isStatus(): Boolean {
-        return this.status;
-    }
+  @Field(() => Number, { nullable: false })
+  join_user!: number;
 
-    public setStatus(status: Boolean): void {
-        this.status = status;
-    }
+  @Field(() => String, { nullable: false })
+  about!: string;
 
-    public getLiked(): Number {
-        return this.liked;
-    }
+  @Field(() => [String], { nullable: false })
+  hashtag!: string[];
 
-    public setLiked(liked: Number): void {
-        this.liked = liked;
-    }
+  @Field(() => Date, { nullable: false })
+  updated_at!: Date;
 
-    public getStart_date(): String {
-        return this.start_date;
-    }
+  @Field(() => Date, { nullable: false })
+  created_at!: Date;
 
-    public setStart_date(start_date: String): void {
-        this.start_date = start_date;
-    }
-
-    public getPlace(): String {
-        return this.place;
-    }
-
-    public setPlace(place: String): void {
-        this.place = place;
-    }
-
-    public getChat_link(): String {
-        return this.chat_link;
-    }
-
-    public setChat_link(chat_link: String): void {
-        this.chat_link = chat_link;
-    }
-
-    public getTotal_user(): Number {
-        return this.total_user;
-    }
-
-    public setTotal_user(total_user: Number): void {
-        this.total_user = total_user;
-    }
-
-    public getJoin_user(): Number {
-        return this.join_user;
-    }
-
-    public setJoin_user(join_user: Number): void {
-        this.join_user = join_user;
-    }
-
-    public getAbout(): String {
-        return this.about;
-    }
-
-    public setAbout(about: String): void {
-        this.about = about;
-    }
-
-    public getHashtag(): Object {
-        return this.hashtag;
-    }
-
-    public setHashtag(hashtag: Object): void {
-        this.hashtag = hashtag;
-    }
-
-    public getEdit_time(): String {
-        return this.edit_time;
-    }
-
-    public setEdit_time(edit_time: String): void {
-        this.edit_time = edit_time;
-    }
-
-    public getCreate_time(): String {
-        return this.create_time;
-    }
-
-    public setCreate_time(create_time: String): void {
-        this.create_time = create_time;
-    }
-
-    public isDeleted(): Boolean {
-        return this.deleted;
-    }
-
-    public setDeleted(deleted: Boolean): void {
-        this.deleted = deleted;
-    }
-
-    printInfoGroup = (): void => {
-        Logger.log({
-            group_seq : this.seq,
-            user_seq : this.user_seq,
-            group_name : this.name,
-            group_kind : this.kind,
-            group_status : this.status,
-            group_liked : this.liked,
-            group_start_date : this.start_date,
-            group_place : this.place,
-            group_chat_link : this.chat_link,
-            group_total_user : this.total_user,
-            group_join_user : this.join_user,
-            group_about : this.about,
-            group_hashtag : this.hashtag,
-            group_edit_time : this.edit_time,
-            group_create_time : this.create_time
-        });
-    }
+  @Field(() => Boolean, { nullable: false })
+  deleted!: boolean;
 }
-*/
