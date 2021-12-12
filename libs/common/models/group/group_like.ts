353f9a517303f2ccf6c75aee75992da4d7237dbd
Gwon Min Je <gwon713@gmail.com>
2021-12-12 22:18:04 +0900

    feat: group like model

--- libs/common/models/group/group_like.ts
+++ libs/common/models/group/group_like.ts
@@ -1,47 +1,13 @@
-/*
-import { Logger } from '@nestjs/common';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
-export class GroupLike {
-  private seq: Number;
-  private user_seq: Number;
-  private group_seq: Number;
+@ObjectType()
+export class DdudoGroupLike {
+  @Field(() => ID, { nullable: false })
+  id!: string;
 
-  constructor(seq: Number, user_seq: Number, group_seq: Number) {
-    this.seq = seq;
-    this.user_seq = user_seq;
-    this.group_seq = group_seq;
-  }
+  @Field(() => String, { nullable: false })
+  userId!: string;
 
-  public getSeq(): number {
-    return this.seq;
-  }
-
-  public setSeq(seq: Number): void {
-    this.seq = seq;
-  }
-
-  public getUser_seq(): Number {
-    return this.user_seq;
-  }
-
-  public setUser_seq(user_seq: Number): void {
-    this.user_seq = user_seq;
-  }
-
-  public getGroup_seq(): Number {
-    return this.group_seq;
-  }
-
-  public setGroup_seq(group_seq: Number): void {
-    this.group_seq = group_seq;
-  }
-
-  printInfoGroup = (): void => {
-    Logger.log({
-      group_liked_seq: this.seq,
-      user_seq: this.user_seq,
-      group_seq: this.group_seq,
-    });
-  };
+  @Field(() => String, { nullable: false })
+  groupId!: string;
 }
-*/
