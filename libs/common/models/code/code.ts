8f6f53fa3b748275ba42d5ce21573411a92e92b2
Gwon Min Je <gwon713@gmail.com>
2021-12-13 22:28:17 +0900

    feat: code model

--- libs/common/models/code/code.ts
+++ libs/common/models/code/code.ts
@@ -1,64 +1,16 @@
-/*
-import { Logger } from "@nestjs/common";
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
-export class Code{
-    private code_id : String;
-    private code_name : String;
-    private code_etc : String;
-    private use_at : Boolean;
+@ObjectType()
+export class DdudoCode {
+  @Field(() => ID, { nullable: false })
+  id!: string;
 
-    constructor(
-        code_id: String, 
-        code_name: String, 
-        code_etc: String, 
-        use_at: Boolean
-    ) 
-    {
-        this.code_id = code_id
-        this.code_name = code_name
-        this.code_etc = code_etc
-        this.use_at = use_at
-    }
+  @Field(() => String, { nullable: false })
+  name!: string;
 
-    public getCode_id(): String {
-        return this.code_id;
-    }
+  @Field(() => String, { nullable: false })
+  etc!: string;
 
-    public setCode_id(code_id: String): void {
-        this.code_id = code_id;
-    }
-
-    public getCode_name(): String {
-        return this.code_name;
-    }
-
-    public setCode_name(code_name: String): void {
-        this.code_name = code_name;
-    }
-
-    public getCode_etc(): String {
-        return this.code_etc;
-    }
-
-    public setCode_etc(code_etc: String): void {
-        this.code_etc = code_etc;
-    }
-
-    public isUse_at(): Boolean {
-        return this.use_at;
-    }
-
-    public setUse_at(use_at: Boolean): void {
-        this.use_at = use_at;
-    }
-
-    printInfoCode = () : void => {
-        Logger.log({
-            code_id : this.code_id,
-            code_name : this.code_name,
-            code_etc : this.code_etc,
-            use_at : this.use_at
-        });
-    }
-};
-*/
+  @Field(() => Boolean, { nullable: false })
+  use_at!: boolean;
+}
