c212ed3fbacb6240ba9ae3a8298ca523a368ca8a
Gwon Min Je <gwon713@gmail.com>
2021-12-15 23:17:07 +0900

    feat: user resolver init

---
+++ src/user/user.resolver.ts
@@ -0,0 +1,26 @@
+import {
+  Args,
+  Int,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { DdudoUser } from 'libs/common/models/user';
+import { UserService } from './user.service';
+
+@Resolver((of) => DdudoUser)
+export class userResolver {
+  constructor(private userService: UserService) {}
+
+  @Query((returns) => DdudoUser)
+  async user(@Args('id', { type: () => Int }) id: number) {
+    return this.userService.getHealth();
+  }
+
+  @ResolveField()
+  async posts(@Parent() user: DdudoUser) {
+    const { id } = user;
+    return this.userService.getHealth();
+  }
+}
