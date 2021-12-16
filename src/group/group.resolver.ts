07100c8b8e417631e544a371b238183c207c3e59
Gwon Min Je <gwon713@gmail.com>
2021-12-16 22:51:43 +0900

    feat: group resolver init

---
+++ src/group/group.resolver.ts
@@ -0,0 +1,26 @@
+import {
+  Args,
+  Int,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { GroupService } from './group.service';
+import { DdudoGroup } from 'libs/common/models/group/group';
+
+@Resolver((of) => DdudoGroup)
+export class groupResolver {
+  constructor(private groupService: GroupService) {}
+
+  @Query((returns) => DdudoGroup)
+  async user(@Args('id', { type: () => Int }) id: number) {
+    return this.groupService.getHealth();
+  }
+
+  @ResolveField()
+  async posts(@Parent() group: DdudoGroup) {
+    const { id } = group;
+    return this.groupService.getHealth();
+  }
+}
