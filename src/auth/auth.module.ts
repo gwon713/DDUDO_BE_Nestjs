7ffeeb6b1031184127c8bdb2282c176913d577ce
Gwon Min Je <gwon713@gmail.com>
2021-12-25 13:46:40 +0900

    fix: auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UserService],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly userService: UserService) {}
   async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
+    const user = await this.userService.userLogin(username);
     if (user && user.password === pass) {
       const { password, ...result } = user;
       return result;
