544b5879f465577a6df5726f4079b2baca20ad84
Gwon Min Je <gwon713@gmail.com>
2022-01-01 12:45:32 +0900

    feat: query init

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { DdudoUser } from 'libs/common/models/user';
+import { Connection, QueryBuilder } from 'typeorm';
 
 @Injectable()
 export class UserService {
+  constructor(
+    private readonly connection: Connection,
+    private readonly logger: Logger,
+  ) {
+    this.logger = new Logger(UserService.name);
+  }
+
   getHealth(): string {
     return 'user health';
   }
 
   async userLogin(): Promise<DdudoUser> {
+    const queryRunner = this.connection.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      await queryRunner.commitTransaction();
+    } catch (err) {
+      // since we have errors lets rollback the changes we made
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // you need to release a queryRunner which was manually instantiated
+      await queryRunner.release();
+    }
     return;
   }
 
