0597c1d25780917a9e826104f09e1c2c570a24c7
Gwon Min Je <gwon713@gmail.com>
2022-01-05 22:25:43 +0900

    feat: jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UserService],
+  imports: [
+    UserService,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
