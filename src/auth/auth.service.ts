045f29dc09a4a75969cb7304bec0ed2f696c4d99
Gwon Min Je <gwon713@gmail.com>
2022-01-10 22:22:04 +0900

    feat: encrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { DdudoUser } from 'libs/common/models/user';
 import { DdudoUserEntity } from 'libs/database/entities';
 import { UserService } from '../user/user.service';
+import { createCipheriv, randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
 
 @Injectable()
 export class AuthService {
@@ -15,5 +17,21 @@ export class AuthService {
       return user;
     }
     return null;
+  }
+
+  async encrypt() {
+    const iv = randomBytes(16);
+    const password = 'Password used to generate key';
+
+    // The key length is dependent on the algorithm.
+    // In this case for aes256, it is 32 bytes.
+    const key = (await promisify(scrypt)(password, 'salt', 32)) as Buffer;
+    const cipher = createCipheriv('aes-256-ctr', key, iv);
+
+    const textToEncrypt = 'Nest';
+    const encryptedText = Buffer.concat([
+      cipher.update(textToEncrypt),
+      cipher.final(),
+    ]);
   }
 }
