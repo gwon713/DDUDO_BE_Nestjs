458f6bd9448ca00f044906f08cdbc0cb15806bd9
Gwon <62048410+gwon713@users.noreply.github.com>
2022-02-03 21:26:27 +0900

    fix: db config

--- libs/database/database.module.ts
+++ libs/database/database.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { DdudoUserEntity } from './entities/user.entity';
 import { DdudoGroupEntity } from './entities/group.entity';
 import { GraphQLModule } from '@nestjs/graphql';
@@ -8,6 +8,7 @@ import { DdudoCodeDetailEntity } from './entities/code_detail.entity';
 import { DdudoCodeEntity } from './entities/code.entity';
 import { DdudoGroupLikeEntity } from './entities/group_like.entity';
 import { join } from 'path';
+import { DdudoConfigService } from '../common/config/ddudo-config.service';
 
 @Module({
   imports: [
@@ -15,21 +16,35 @@ import { join } from 'path';
       isGlobal: true,
       envFilePath: `./env/${process.env.NODE_ENV}.env`,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT),
-      username: process.env.PG_USER,
-      database: process.env.PG_DATABASE,
-      password: process.env.PG_PASSWORD,
-      entities: [
-        DdudoUserEntity,
-        DdudoGroupEntity,
-        DdudoGroupLikeEntity,
-        DdudoCodeEntity,
-        DdudoCodeDetailEntity,
-      ],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (
+        config: DdudoConfigService,
+      ): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions => ({
+        type: 'postgres',
+        host: config.dbHost,
+        port: config.dbPort,
+        username: config.dbUser,
+        password: config.dbPassword,
+        database: config.dbDatabase,
+        keepConnectionAlive: true,
+        entities: [
+          DdudoUserEntity,
+          DdudoGroupEntity,
+          DdudoGroupLikeEntity,
+          DdudoCodeEntity,
+          DdudoCodeDetailEntity,
+        ],
+        migrations: [],
+        subscribers: [],
+        synchronize: true,
+        extra: {
+          max: 5,
+          maxUses: 5000,
+          connectionTimeoutMillis: 5000,
+          idleTimeoutMillis: 1000,
+        },
+      }),
+      inject: [DdudoConfigService],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), './schema.gql'),
