bc36bacdbe0c74de7bbea0baf6743afe45908938
Gwon <62048410+gwon713@users.noreply.github.com>
2022-02-03 21:20:16 +0900

    feat: config db config

--- libs/common/config/ddudo-config.service.ts
+++ libs/common/config/ddudo-config.service.ts
@@ -8,6 +8,22 @@ export class DdudoConfigService {
     return this.config.get<string>('PG_HOST', 'localhost');
   }
 
+  get dbUser(): string {
+    return this.config.get<string>('PG_USER', 'admin');
+  }
+
+  get dbDatabase(): string {
+    return this.config.get<string>('PG_DATABASE', 'ddudo');
+  }
+
+  get dbPassword(): string {
+    return this.config.get<string>('PG_PASSWORD', 'ddudo123');
+  }
+
+  get dbPort(): number {
+    return this.config.get<number>('PG_PORT', 5432);
+  }
+
   get jwtSecret(): string {
     return this.config.get<string>('JWT_SECRET', 'ddudo_jwt_secret');
   }
